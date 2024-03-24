25ce8855f4a53bb32bb50458a073f4900f1fee13
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-03-24 13:15:42 -0600

    ADD methods and refactor

--- Usuario.java
+++ Usuario.java
@@ -14,20 +14,20 @@ public class Usuario {
     }
     
     public void iniciarSesion(){
+        int count=0;
         Scanner leer = new Scanner(System.in);
         Gerente gerente = new Gerente();
         Administrador admin = new Administrador();
 
         do{
-
             
         System.out.println("--- Bienvenido ---");
         System.out.print("Ingrese su usuario: ");
         this.user = leer.nextLine();
         System.out.print("Ingrese su contraseña: ");
         this.pass = leer.nextLine();
-
         
+        count+=1;
 
         }while(count != 3);
         
