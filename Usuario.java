26a3edb48f097aee7ac20e2a56b420f91af11341
IDS-Mandujano <jesusgutierrez191105@gmail.com>
2024-03-24 16:06:50 -0600

    VALIDATIONS in Login

--- Usuario.java
+++ Usuario.java
@@ -18,43 +18,59 @@ public class Usuario {
         Gerente gerente = new Gerente();
         Administrador admin = new Administrador();
 
+        Boolean succes = false;
         String rol = null;
+        int count = 3;
 
         String userG = gerente.getUser(), userA = admin.getUser();
         String passG = gerente.getPass(), passA = admin.getPass();
         String rolG = gerente.getRol(), rolA = admin.getRol();
 
         
-            
-        System.out.println("--- Bienvenido ---");
-        System.out.println("escriba su Rol: \nGerente / Admin");
-        rol = leer.nextLine();
-        System.out.print("Ingrese su usuario: ");
-        this.user = leer.nextLine();
-        System.out.print("Ingrese su contraseña: ");
-        this.pass = leer.nextLine();
+        do{
 
-        if (rol.equals(rolG)) {
-            if(!userG.equals(this.user) && !passG.equals(this.pass)){
-                System.out.println("Usuario incorrecto y contraseña incorrectos");
-            }else if(!userG.equals(this.user)){
-                System.out.println("Usuario incorrecto");
-            }else if(!passG.equals(this.pass)){
-                System.out.println("Contraseña incorrecta");
+            System.out.println("--- Bienvenido ---");
+            System.out.println("Intentos: "+count);
+            System.out.println("escriba su Rol: \nGerente / Admin");
+            rol = leer.nextLine();
+            System.out.print("Ingrese su usuario: ");
+            this.user = leer.nextLine();
+            System.out.print("Ingrese su contraseña: ");
+            this.pass = leer.nextLine();
+    
+            if (rol.equals(rolG)) {
+                if(!userG.equals(this.user) && !passG.equals(this.pass)){
+                    System.out.println("Usuario incorrecto y contraseña incorrectos");
+                    count-=1;
+                }else if(!userG.equals(this.user)){
+                    System.out.println("Usuario incorrecto");
+                    count-=1;
+                }else if(!passG.equals(this.pass)){
+                    System.out.println("Contraseña incorrecta");
+                    count-=1;
+                }else if (this.user.equals(userG) && this.pass.equals(passG)) {
+                    succes = true;
+                }
+            }else if (rol.equals(rolA)) {
+                if(!userA.equals(this.user) && !passA.equals(this.pass)){
+                    System.out.println("Usuario incorrecto y contraseña incorrectos");
+                    count-=1;
+                }else if(!userA.equals(this.user)){
+                    System.out.println("Usuario incorrecto");
+                    count-=1;
+                }else if(!passA.equals(this.pass)){
+                    System.out.println("Contraseña incorrecta");
+                    count-=1;
+                }else if (this.user.equals(userA) && this.pass.equals(passA)) {
+                    succes = true;
+                }
             }
-        }else if (rol.equals(rolA)) {
-            if(!userA.equals(this.user) && !passA.equals(this.pass)){
-                System.out.println("Usuario incorrecto y contraseña incorrectos");
-            }else if(!userA.equals(this.user)){
-                System.out.println("Usuario incorrecto");
-            }else if(!passA.equals(this.pass)){
-                System.out.println("Contraseña incorrecta");
-            }
-        }
+        }while(count!=0 && succes == false);
 
-        if (rol.equals(rolG)) {
+
+        if (rol.equals(rolG) && succes == true) {
             gerente.verMenu();
-        }else if (rol.equals(rolA)) {
+        }else if (rol.equals(rolA) && succes == true) {
             admin.verMenu();
         }
 
